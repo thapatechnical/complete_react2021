c86cbf60f093713ffa1d01ef64640c1b9fec4ac6
Shaish Guni <70198546+shaishguni@users.noreply.github.com>
2022-09-13 06:01:31 +0545

    invalid class syntax

--- src/component/Hooks/useReducer.js
+++ src/component/Hooks/useReducer.js
@@ -22,14 +22,14 @@ const UseReducer = () => {
     <>
       <div className="center_div">
         <p>{state}</p>
-        <div class="button2" onClick={() => dispatch({ type: "INCR" })}>
+        <div className="button2" onClick={() => dispatch({ type: "INCR" })}>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           INCR
         </div>
-        <div class="button2" onClick={() => dispatch({ type: "DECR" })}>
+        <div className="button2" onClick={() => dispatch({ type: "DECR" })}>
           <span></span>
           <span></span>
           <span></span>
