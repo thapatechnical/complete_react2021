8fc71afb2614185d9d527b8c6d963affc6cd9015
Shaish Guni <70198546+shaishguni@users.noreply.github.com>
2022-09-13 05:57:42 +0545

    invalid class syntax

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -9,7 +9,7 @@ const UseState = () => {
     <>
       <div className="center_div">
         <p>{myNum}</p>
-        <div class="button2" onClick={() => setMyNum(myNum + 1)}>
+        <div className="button2" onClick={() => setMyNum(myNum + 1)}>
           <span></span>
           <span></span>
           <span></span>
